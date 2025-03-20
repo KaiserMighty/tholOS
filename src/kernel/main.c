48f5ddc6e34d5004c06ee9583dac63018514711f
Kaiser Mighty <unikmey@gmail.com>
2025-03-19 23:03:11 -0700

    Hardware Interrupts

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -2,6 +2,7 @@
 #include "stdio.h"
 #include "memory.h"
 #include <hal/hal.h>
+#include <arch/i686/irq.h>
 
 extern uint8_t __bss_start;
 extern uint8_t __end;
@@ -10,16 +11,23 @@ void divide_by_zero();
 void overflow();
 void segment_not_present();
 
+void timer(Registers* regs)
+{
+    printf("Timer Interrupt!\n");
+}
+
 void __attribute__((section(".entry"))) start(uint16_t bootDrive)
 {
     memset(&__bss_start, 0, (&__end) - (&__bss_start));
     HAL_Initialize();
     clrscr();
     printf("Hello world!\n");
-    
+
     // divide_by_zero();
     // overflow();
-    segment_not_present();
+    // segment_not_present();
+    
+    i686_IRQ_RegisterHandler(0, timer);
 
 end:
     for (;;);
