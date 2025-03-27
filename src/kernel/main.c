f12737afccd596565e66af585bb6297321757fa3
Kaiser Mighty <unikmey@gmail.com>
2025-03-27 15:52:11 -0700

    ELF Binary Debugging

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -5,9 +5,6 @@
 #include <arch/i686/irq.h>
 #include <debug.h>
 
-extern uint8_t __bss_start;
-extern uint8_t __end;
-
 extern void _init();
 
 void divide_by_zero();
@@ -19,9 +16,8 @@ void timer(Registers* regs)
     printf("Timer Interrupt!\n");
 }
 
-void __attribute__((section(".entry"))) start(uint16_t bootDrive)
+void start(uint16_t bootDrive)
 {
-    memset(&__bss_start, 0, (&__end) - (&__bss_start));
     _init();
     HAL_Initialize();
 
